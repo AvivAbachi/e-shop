da677623c15d66fb8a96cce95bbb3ea94d69f1de
aviv <avivabachi@gmail.com>
2023-03-19 12:31:49 +0200

    set up server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const data = require('./data');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/api/v1/products', (req, res) => res.send(data.products));
+app.get('/api/v1/products/:id', (req, res) => {
+	const product = data.products.find((p) => p.id === req.params.id);
+	if (product) return res.send(product);
+	return res.status(404).send('Product not found');
+});
+
+app.listen(port, () => console.log(`E-Shop server listening on port ${port}!`));
